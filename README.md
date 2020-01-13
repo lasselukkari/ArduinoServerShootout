8e1d84898816b3c2254e25200f913d0fdefc881c
Lasse Lukkari <lasse.lukkari@gmail.com>
2020-01-13 20:34:21 +0200

    Initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-# Arduino HTTP Sever Shootout
-An attempt to measure the write performance of three different Arduino web server frameworks.
-
-See results at: http://lasselukkari.github.io/ArduinoServerShootout
---
+++ precache-manifest.060a9b23ea3a82cb4f8a0c16074e17d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda83ae2c6ad7ddb1ae41a6f3dbb8060",
+    "url": "/ArduinoServerShootout/index.html"
+  },
+  {
+    "revision": "a48943ad50f1840206e4",
+    "url": "/ArduinoServerShootout/static/css/main.5c652370.chunk.css"
+  },
+  {
+    "revision": "2600c3fc5bbfa551f52f",
+    "url": "/ArduinoServerShootout/static/js/2.4fcfdfab.chunk.js"
+  },
+  {
+    "revision": "efe80d506e138c343a03a1541511a2f3",
+    "url": "/ArduinoServerShootout/static/js/2.4fcfdfab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a48943ad50f1840206e4",
+    "url": "/ArduinoServerShootout/static/js/main.3565ad3a.chunk.js"
+  },
+  {
+    "revision": "7c038879ab1d3b499a09",
+    "url": "/ArduinoServerShootout/static/js/runtime-main.f181135c.js"
+  }
+]);
